aea5c25edead20a7b30348d5698098976d2655b3
danilourze <danilo.urze@outlook.com>
2023-11-23 21:07:16 -0300

    css

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,10 @@
 import { } from 'react'
 import Logo from '../assets/Captura_de_tela_2023-11-22_200811-removebg-preview.png';
-import Enfeite from '../assets/Enfeite.svg';
-import Enfeite2 from '../assets/Enfeite2.svg';
-import ImagemIngredientes from '../assets/ImagemIngredientes.svg';
-import ImagemReceitas from '../assets/ImagemReceitas.svg';
-import Ruas from '../assets/Ruas.svg';
+import Enfeite from '../assets/captura de tela 2023-11-23 202133.png';
+import Enfeite2 from '../assets/bagulho vermelho .png';
+import ImagemIngredientes from '../assets/medico1.png';
+import ImagemReceitas from '../assets/medico2.png';
+import Ruas from '../assets/medico3.png';
 import { Link } from 'react-router-dom';
 
 function Home() {
@@ -15,7 +15,7 @@ function Home() {
                     <img src={Logo} alt='Logo da empresa' className='sobre_nos-logo'/>
                     <div className="sobre_nos-textos">
                         <h1 className="sobre_nos-textos-titulo">Problema</h1>
-                        <p className="sobre_nos-textos-texto">O projeto KIT ON nasceu da ideia de diminuir a quantidade de lixo nas ruas. E para fazer isso, escolhemos o método de conscientizar as pessoas, porém, não entregando informações massantes e desncessárias para o público, e sim, de maneira interativa. De forma que os usuários teriam um meio de administrar seus alimentos e receber informações sobre o formas de utiliza-lo e a maneira correta de descarta-lo, junto de avisos sobre a importância o descarte de lixo correto.</p>
+                        <p className="sobre_nos-textos-texto">A gestão manual de registros hospitalares representa um desafio tanto para os médicos quanto para os pacientes. Especialmente os pacientes mais idosos ou com prioridades não podem passar horas em filas de espera. Diante disso, torna-se evidente a necessidade crescente de uma solução que integre registros eletrônicos e IoT para proporcionar uma gestão eficiente, acessível e proativa da saúde. Essa abordagem contribuirá significativamente para auxiliar nas tomadas de decisões e superar as dificuldades no monitoramento contínuo da saúde dos pacientes.</p>
                     </div>
                 </section>
 
@@ -23,14 +23,14 @@ function Home() {
                     <article className='importancia_div'>
                         <div className='importancia_textos'>
                             <h1 className='importancia_titulo'>Objetivo</h1>
-                            <p className="importancia-texto">Descartar o lixo de maneira correta pode trazer diversos benefícios para toda a humanidade. Quando se é separado o lixo de maneira correta, se facilita os meios de tratamento deles e diminuem a chance de ter impactos nocivos para o ambiente.
+                            <p className="importancia-texto">Desenvolver uma solução inovadora de gestão hospitalar que integre registros eletrônicos, IoT e medicina online. Buscamos proporcionar uma administração eficiente e acessível da saúde, reduzindo o tempo de espera, especialmente para pacientes vulneráveis como os idosos. A implementação da medicina online permitirá consultas virtuais, melhorando o acesso à saúde. O foco é contribuir para decisões informadas e ágeis, superando desafios na gestão manual de registros e promovendo uma transformação positiva na prestação de serviços de saúde, com ênfase na segurança da informação e privacidade dos pacientes. 
                             </p>
                         </div>
                         <img src={Enfeite} className='importancia_enfeite' alt='Enfeite visual' />
                     </article>
                     <article className='importancia_div'>
                         <img src={Enfeite} className='importancia_enfeite2' alt='Enfeite visual' />
-                        <p className="importancia-texto2">Alguns dos benefícios do descarte apropriado do lixo seria a diminuição da poluição, diminuição de situações de alagamento, diminuição do gasto com limpezas urbanas, valorização dos pontos turísticos, Descontaminação do lençol freático.</p>
+                        <p className="importancia-texto2">Redefinindo o Cuidado Hospitalar com Tecnologia, Acesso Rápido e Compromisso com sua Segurança.</p>
                     </article>
                 </section>
 
@@ -38,7 +38,7 @@ function Home() {
                 <section className="como_trabalhamos">
                     <div className="como_trabalhamos-info">
                         <h1 className="como_trabalhamos-titulo">Como funcionamos?</h1>
-                        <p className="como_trabalhamos-texto">Ao acessar a rede KIT ON, você poderá registrar todos os alimentos presentes na sua casa, sendo necessário junto deles adicionar suas respectivas datas de validade. Após isso, nós separaremos receitas que podem ser cozinhadas utilizando os ingredientes registrados.</p>
+                        <p className="como_trabalhamos-texto">A MediConnect, nossa solução inovadora, integra registros eletrônicos e dispositivos IoT para transformar a gestão hospitalar. Os registros eletrônicos oferecem segurança e interoperabilidade, com acesso restrito a profissionais autorizados. Dispositivos IoT monitoram pressão arterial, frequência cardíaca e glicose, transmitindo dados automaticamente aos registros eletrônicos. Uma plataforma analítica processa e alerta sobre condições médicas precoces. O aplicativo para pacientes e profissionais proporciona acesso aos registros e recursos educativos, incentivando a autogestão. Nosso trabalho inclui o desenvolvimento do sistema, dispositivos IoT, plataforma analítica e aplicativo, priorizando medidas de segurança. Espera-se um MediConnect funcional, permitindo acesso em tempo real a dados, monitoramento contínuo da saúde, detecção precoce e engajamento proativo na autogestão da saúde.</p>
                         <p className="como_trabalhamos-texto2">E caso a data de validade se aproxime, você receberá a avisos, a forma correta de descarta-lo e a importância disso.</p>
                         <p className="como_trabalhamos-texto3">Para aproveitar nossos benefícios, basta entrar na rede KIT ON:</p>
                         <div className="como_trabalhamos-botoes">
@@ -54,15 +54,15 @@ function Home() {
                     <div className="cards">
                         <article className="card">
                             <img src={ImagemReceitas} alt="Imagem-Desbrave novas receitas" className="imagem-card"></img>
-                            <p className="card-texto">Desbrave novas receitas</p>
+                            <p className="card-texto">Velocidade no atendimento</p>
                         </article>
                         <article className="card">
                             <img src={ImagemIngredientes} alt="Imagem-Controle seus ingredientes" className="imagem-card"></img>
-                            <p className="card-texto">Controle seus ingredientes</p>
+                            <p className="card-texto">Controle de pacientes</p>
                         </article>
                         <article className="card">
                             <img src={Ruas} alt="Imagem-Ajude a limpar as ruas" className="imagem-card"></img>
-                            <p className="card-texto">Ajude a limpar as ruas</p>
+                            <p className="card-texto">Acesso remoto</p>
                         </article>
                     </div>
                 </section>
